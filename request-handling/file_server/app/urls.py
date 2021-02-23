c826d03ac8562d2479a22e9f2d6e401f7af1cee3
jackkr132 <72446270+jackkr132@users.noreply.github.com>
2021-02-23 14:25:42 +0300

    Update urls.py

--- request-handling/file_server/app/urls.py
+++ request-handling/file_server/app/urls.py
@@ -1,12 +1,13 @@
-from django.urls import path
+from django.urls import path, converters, register_converter
+from file_server.app.converters import DateConverter
+from file_server.app.views import file_list, file_content
 
-# Определите и зарегистрируйте конвертер для определения даты в урлах и наоборот урла по датам
 
+register_converter(DateConverter, 'date')
 
 urlpatterns = [
-    # Определите схему урлов с привязкой к отображениям .views.file_list и .views.file_content
-    # path(..., name='file_list'),
-    # path(..., name='file_list'),    # задайте необязательный параметр "date"
-                                      # для детальной информации смотрите HTML-шаблоны в директории templates
-    # path(..., name='file_content'),
+    path('', file_list, name='file_list'),
+    # пришлось использовать переменную date потому что такое имя прописано в шаблоне, который менять не желательно
+    path('<date:date>/', file_list, name='file_list'),
+    path('<str:name>/', file_content, name='file_content')
 ]
