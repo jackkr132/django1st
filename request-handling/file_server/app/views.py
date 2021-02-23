2b4095b85c8b21a7f070c9621415490096eac4a5
jackkr132 <72446270+jackkr132@users.noreply.github.com>
2021-02-23 14:25:57 +0300

    Update views.py

--- request-handling/file_server/app/views.py
+++ request-handling/file_server/app/views.py
@@ -1,29 +1,29 @@
-import datetime
-
+from datetime import datetime, date
+import os
+from django.conf import settings
+from django.http import HttpResponseNotFound
 from django.shortcuts import render
 
 
-def file_list(request):
+def file_list(request, date: date = None):
     template_name = 'index.html'
-    
-    # Реализуйте алгоритм подготавливающий контекстные данные для шаблона по примеру:
-    context = {
-        'files': [
-            {'name': 'file_name_1.txt',
-             'ctime': datetime.datetime(2018, 1, 1),
-             'mtime': datetime.datetime(2018, 1, 2)}
-        ],
-        'date': datetime.date(2018, 1, 1)  # Этот параметр необязательный
-    }
-
-    return render(request, template_name, context)
+    filenames_list = os.listdir(settings.FILES_PATH)
+    files_list = []
+    for file_name in filenames_list:
+        stat = os.stat(os.sep.join([settings.FILES_PATH, file_name]))
+        dt_ctime = datetime.fromtimestamp(stat.st_ctime)
+        dt_mtime = datetime.fromtimestamp(stat.st_mtime)
+        if date and not (date == dt_ctime.date() or date == dt_mtime.date()):
+            continue
+        files_list.append({'name': file_name, 'ctime': dt_ctime, 'mtime': dt_mtime})
+    return render(request, template_name, context={'files': files_list, 'date': date})
 
 
 def file_content(request, name):
-    # Реализуйте алгоритм подготавливающий контекстные данные для шаблона по примеру:
-    return render(
-        request,
-        'file_content.html',
-        context={'file_name': 'file_name_1.txt', 'file_content': 'File content!'}
-    )
-
+    template_name = 'file_content.html'
+    full_name = os.sep.join([settings.FILES_PATH, name])
+    if os.path.isfile(full_name):
+        with open(full_name, encoding='utf8', mode='r') as f:
+            content = ''.join(f.readlines())
+        return render(request, template_name, context={'file_name': name, 'file_content': content})
+    return HttpResponseNotFound('File not found')
